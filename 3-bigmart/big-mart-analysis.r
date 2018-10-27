d585f74a0c1e235804c83ba1e941e4dc38672bc3
baigh.yasar@gmail.com <baigh.yasar@gmail.com>
2018-10-27 16:24:31 +0530

    big-mart sales

--- 3-bigmart/big-mart-analysis.r
+++ 3-bigmart/big-mart-analysis.r
@@ -34,11 +34,131 @@ combi$Item_Fat_Content[combi$Item_Fat_Content == "low fat"] = "Low Fat"
 combi$Item_Fat_Content[combi$Item_Fat_Content == "reg"] = "Regular"
 
 #Outlet size has blank values
+#checking blank values in weight
+sum(is.na(combi$Item_Weight))
+
+#filling the blank values of weight, with the mean values of its product types
+missing_index = which(is.na(combi$Item_Weight))
+for(i in missing_index){
+  
+  item = combi$Item_Identifier[i]
+  combi$Item_Weight[i] = mean(combi$Item_Weight[combi$Item_Identifier == item], na.rm = T)
+}
+
+sum(is.na(combi$Item_Weight))
+
+#filling the blank/0 values of Item_Visibility, with the mean values of its product types
+zero_index = which(combi$Item_Visibility == 0)
+for(i in zero_index){
+  
+  item = combi$Item_Identifier[i]
+  combi$Item_Visibility[i] = mean(combi$Item_Visibility[combi$Item_Identifier == item], na.rm = T)
+}
+
+#some-times, we need additional categorization which is not in 
+#the dataset, 4 deep analysis.
+#such as categorizing above items into perishable and non-perishable
+perishable = c("Breads", "Breakfast", "Dairy", "Fruits and Vegetables", "Meat", "Seafood")
+non_perishable = c("Baking Goods", "Canned", "Frozen Foods", "Hard Drinks", "Health and Hygiene", "Household", "Soft Drinks")
+
+#adding new category perishable and non-perishable
+combi[,Item_Type_new := ifelse(Item_Type %in% perishable, "perishable",
+                               ifelse(Item_Type %in% non_perishable, "non_perishable", "not_sure"))]
+
+
+ggplot(combi %>% group_by(Item_Type_new) %>% summarise(Count = n())) + 
+  geom_bar(aes(Item_Type_new, Count), stat = "identity", fill = "coral1")
+
+# combi$Item_Identifier has each unique-id , for each product,
+#but with prefix FD for food, DR-Drinks, NC-NonConumables
+
+#viewing, item-type group_by identity-prefix
+table(combi$Item_Type, substr(combi$Item_Identifier, 1, 2))  
+#adding new category perishable and non-perishable
+combi[,Item_category := substr(combi$Item_Identifier, 1, 2)]  
+  
+  
+#replacing the fat-content column with Non-Edible, where category is NC
+#since non-edible , does not have fat
+combi$Item_Fat_Content[combi$Item_category == "NC"] = "Non-Edible" 
+
+# years of operation for outlets
+combi[,Outlet_Years := 2018 - Outlet_Establishment_Year]
+combi$Outlet_Establishment_Year = as.factor(combi$Outlet_Establishment_Year)
+# Price per unit weight
+combi[,price_per_unit_wt := Item_MRP/Item_Weight]
+
+
+# creating new independent variable - Item_MRP_clusters
+combi[,Item_MRP_clusters := ifelse(Item_MRP < 69, "1st", 
+                                   ifelse(Item_MRP >= 69 & Item_MRP < 136, "2nd",
+                                          ifelse(Item_MRP >= 136 & Item_MRP < 203, "3rd", "4th")))]
+
+										  
+#completed, filling blank values and added new categories for deep analysis
+#------------------------------------------------------------------------------------------
 
 
 
+#Label encoding,
+#replacing categorical values with numerical values
+combi[,Outlet_Size_num := ifelse(Outlet_Size == "Small", 0,
+                                 ifelse(Outlet_Size == "Medium", 1, 2))]
+								 
+combi[,Outlet_Location_Type_num := ifelse(Outlet_Location_Type == "Tier 3", 0,
+                                          ifelse(Outlet_Location_Type == "Tier 2", 1, 2))]
+# removing categorical variables after label encoding
+combi[, c("Outlet_Size", "Outlet_Location_Type") := NULL]
 
-print("last-line")
+
+## One Hot Encoding
+
+ohe = dummyVars("~.", data = combi[,-c("Item_Identifier", "Outlet_Establishment_Year", "Item_Type")], fullRank = T)
+ohe_df = data.table(predict(ohe, combi[,-c("Item_Identifier", "Outlet_Establishment_Year", "Item_Type")]))
+
+combi = cbind(combi[,"Item_Identifier"], ohe_df)
+#-------------------------------------------------------------------------------------------------------------------------
+
+
+
+## Remove skewness
+
+combi[,Item_Visibility := log(Item_Visibility + 1)] # log + 1 to avoid division by zero
+combi[,price_per_unit_wt := log(price_per_unit_wt + 1)]
+
+"
+Scaling numeric predictors
+Let’s scale and center the numeric variables to make them have a mean of zero, standard deviation of one and scale of 0 to 1. Scaling and centering is required for linear regression models.
+
+"
+num_vars = which(sapply(combi, is.numeric)) # index of numeric features
+num_vars_names = names(num_vars)
+combi_numeric = combi[,setdiff(num_vars_names, "Item_Outlet_Sales"), with = F]
+prep_num = preProcess(combi_numeric, method=c("center", "scale"))
+combi_numeric_norm = predict(prep_num, combi_numeric)
+
+
+# binding with combi data once again after scaling
+combi[,setdiff(num_vars_names, "Item_Outlet_Sales") := NULL] # removing numeric independent variables
+combi = cbind(combi, combi_numeric_norm)
+
+# splitting train and test data once again
+train = combi[1:nrow(train)]
+test = combi[(nrow(train) + 1):nrow(combi)]
+test[,Item_Outlet_Sales := NULL] # removing Item_Outlet_Sales as it contains only NA for test dataset
+
+#-------------------------
+
+# Correlation between each colummns
+cor_train = cor(train[,-c("Item_Identifier")])
+corrplot(cor_train, method = "pie", type = "lower", tl.cex = 0.9)
+"
+The correlation plot above shows correlation between all the possible pairs of variables in out data. The correlation between any two variables is represented by a pie. A blueish pie indicates positive correlation and reddish pie indicates negative correlation. The magnitude of the correlation is denoted by the area covered by the pie.
+
+Variables price_per_unit_wt and Item_Weight are highly correlated as the former one was created from the latter. Similarly price_per_unit_wt and Item_MRP are highly correlated for the same reason.
+
+"
+print(combi$Outlet_Establishment_Year)
 
 
 
